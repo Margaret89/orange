601b95b55615d6f17d42211bc4660a7c46c37683
katyush14 <katyushina89@gmail.com>
2020-03-04 17:55:04 +0300

    Добавлены формы

--- public/js/main.js
+++ public/js/main.js
@@ -18,9 +18,37 @@ $(document).ready(function () {
 			$(this).parent().children(".js-unwrap-content").slideUp(400);
 		}
 	});
-	
-	// //---------- Маска для телефона -------------
-	// $.mask.definitions['~'] = "[+-]";
-	// $("#phone").mask("(999) 999-9999");
 
+// Плавный переход к ссылке
+	if ($('.js-link-move').length) {
+		$('body').on('click','.js-link-move', function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top - 50;
+			$('body,html').animate({scrollTop: top}, 1000);
+
+			// Проверка наличия анимации
+			productAnim(1);
+		});
+	}
+
+// Маска для телефона
+	$.mask.definitions['~'] = "[+-]";
+	$('.js-phone').mask("+7 (999) 999-9999");
+
+// Вывод сообщения об успешной отправке в попапе
+	$('.js-valid-form').each(function(){
+		$(this).on('submit',function(e){
+			$.fancybox.close();
+			$.fancybox.open({
+				src  : '#msg-success',
+				type : 'inline',
+				opts : {
+					
+				}
+			});
+			$(this)[0].reset();
+			e.preventDefault();
+		});
+	});
 });
